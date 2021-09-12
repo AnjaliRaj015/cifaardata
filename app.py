b3e5806529c7acccda189323b7507200e69ca9af
Anjali Rajendran <84264801+AnjaliRaj015@users.noreply.github.com>
2021-09-12 15:50:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+from prediction import prediction
+
+# Initializing flask application
+app = Flask(__name__)
+cors = CORS(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', title='Home')
+
+
+@app.route("/predict", methods=["POST"])
+def Req():
+    data = request.files["img"]
+    data.save("img.jpg")
+
+    resp = prediction("img.jpg")
+
+    text = "it is a <b>"+resp['label']
+
+    return text
+if __name__=='__main__':
+    app.run(port=5000,debug=True)
+
---
+++ prediction.py
@@ -0,0 +1,25 @@
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from werkzeug.utils import secure_filename
+import cv2
+import numpy as np
+
+names = '''airplane automobile bird cat deer dog frog horse ship truck'''.split()
+model = load_model('models/cifaar_10.h5')
+
+def prediction(IMG_PATH):
+    img = cv2.imread(IMG_PATH)
+    img = cv2.resize(img, (32, 32))
+    img = img.astype("float") / 255.0
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+
+    r = model.predict(img)
+
+    label = np.argmax(r)
+    labelName = names[label]
+    print("The image is of ", labelName)
+
+    pred = dict()
+    pred['label'] = labelName
+    return pred
